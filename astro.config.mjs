330d0345139ac4c288801e385c89d859ee7fb2a0
slushman <chris@slushman.com>
2026-04-07 23:36:20 -0500

    Add Mizzou URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/mizzou': '/university-of-missouri',
+  },
   vite: {
     plugins: [tailwindcss()]
   }
